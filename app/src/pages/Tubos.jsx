ed08b56a13cdc6ead9b0362a463e6c962e2bcb88
tiagolimar <tiagolima.r2@gmail.com>
2023-12-23 03:25:56 -0300

    corrigindo importacao

--- app/src/pages/Tubos.jsx
+++ app/src/pages/Tubos.jsx
@@ -1,7 +1,7 @@
 import { Table } from "../components/Table/Table.jsx";
 
 function Tubos() {
-    return <Table url="https://jsonplaceholder.typicode.com/users"/>;
+    return <Table url="https://calcula-hidro-server.onrender.com/api/tubos"/>;
 }
 
 export default Tubos;
